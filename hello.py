e365be06fd3219efb8bb97d2ce58e1caa2e68d39
shchotse <shchotse@gmail.com>
2016-02-29 17:49:00 +0000

    conf

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def application(environ, start_response):
 	
 	query_string = environ['QUERY_STRING']
-	response = '/n'.join(query_string.split('&'))
+	response = '\n'.join(query_string.split('&'))
 	start_response('200 OK', [('Content-Type', 'text/plain')])
 	
 	return [ response ]
