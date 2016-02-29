3e827cc62535bb2d22b4d2c3dc93d69d046e4023
shchotse <shchotse@gmail.com>
2016-02-29 18:19:16 +0000

    conf

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
+sudo rm -r /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:application
